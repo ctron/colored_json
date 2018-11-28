b31233b7bba9ef0a71c9ee9493468809c6a3203c
Jens Reimann <jreimann@redhat.com>
2018-11-28 16:47:54 +0100

    drop license header, version 0.2.1

--- src/lib.rs
+++ src/lib.rs
@@ -49,19 +49,6 @@ With a custom color style:
 
 !*/
 
-/*******************************************************************************
- * Copyright (c) 2018 Red Hat Inc
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0
- *
- * SPDX-License-Identifier: EPL-2.0
- *******************************************************************************/
-
 extern crate ansi_term;
 extern crate serde;
 extern crate serde_json;
